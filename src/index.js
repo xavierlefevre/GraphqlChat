0ff473e01b1c7f80f02a88ec165c7f62e620177a
Xavier Lefèvre <xavier.lefevre@me.com>
2017-04-17 10:47:39 +0200

    add js prettier

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default class GraphqlChat extends Component {
+  state = {
+    showChatRoom: false
+  };
+
+  accessChatRoom() {
+    this.setState({
+      showChatRoom: true
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <TouchableOpacity onPress={() => this.accessChatRoom()}>
+          <Text>
+            Access Chat Room
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -23,18 +28,18 @@ export default class GraphqlChat extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
